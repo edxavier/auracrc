464e67dfe0107e857a0b43b9b14b8116988c2756
Eder Xavier Rojas <edxavier05@gmail.com>
2014-08-29 10:27:45 -0600

    3rd statics

--- doc/urls.py
+++ doc/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from doc import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,5 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'doc.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^', include('inicio.urls')),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+    	{ 'document_root' : settings.MEDIA_ROOT  }),
     url(r'^admin/', include(admin.site.urls)),
 )
