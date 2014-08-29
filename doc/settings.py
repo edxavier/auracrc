6c836f76f7517baf546e9846dc880bdc9ec4d8da
Eder Xavier Rojas <edxavier05@gmail.com>
2014-08-29 12:23:41 -0600

    1st db

--- doc/settings.py
+++ doc/settings.py
@@ -59,7 +59,7 @@ WSGI_APPLICATION = 'doc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'NAME': 'dc9urk0oteh2j1',
         'USER': 'yyhlrdealwryou',
